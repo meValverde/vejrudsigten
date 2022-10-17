41217c109dcbe8385b48f097b9bb7746f2371044
mgv <mgv@akait.dk>
2022-10-17 20:53:50 +0200

    Created "GenerateTittle" method

--- Vejrudsigten/Services/WeatherForecast.cs
+++ Vejrudsigten/Services/WeatherForecast.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace Vejrudsigten.Services
@@ -12,9 +13,70 @@ namespace Vejrudsigten.Services
             WeatherService service = new WeatherService();
             var todayInfo = await service.GetTodaysWeather(key, "Kolding");
             var yesterdayInfo = await service.GetYesterdaysWeather(key, "Kolding");
+            return GenerateTitle(yesterdayInfo, todayInfo);
+        }
 
-            String result = "Vejret i Kolding er {0} og der er {1} grader. I går var det {2} og {3} grader";
-            return String.Format(result, todayInfo.Conditions, todayInfo.Temperature, yesterdayInfo.Conditions, yesterdayInfo.Temperature);
+        public static string GenerateTitle(WeatherInfo yesterdayInfo, WeatherInfo todayInfo)
+        {
+            var temperature = "";
+            var condition = "";
+
+            switch (yesterdayInfo.Temperature > todayInfo.Temperature ? 0 : 1)
+            {
+                case 0:
+                    temperature =
+                        $"Layer up!, temperature is decreasing from {yesterdayInfo.Temperature} to {todayInfo.Temperature}";
+                    break;
+                case 1:
+                    temperature =
+                        $"Is not all bad news, temperatures increasing today by {todayInfo.Temperature - yesterdayInfo.Temperature} degrees";
+                    break;
+                default:
+                    temperature = $"Temperatures remain stable at {todayInfo.Temperature} degrees";
+                    break;
+            }
+
+            switch (yesterdayInfo.Conditions == todayInfo.Conditions ? 0 : 1)
+            {
+                case 0 when (todayInfo.Conditions == "Klart vejr"):
+                    condition = " - Skies remain clear";
+                    break;
+                case 0 when (todayInfo.Conditions == "Regn"):
+                    condition = " - Wellingtons are also needed today";
+                    break;
+                case 0 when (todayInfo.Conditions == "Sne"):
+                    condition = " - Whoever is praying for snow, please stop";
+                    break;
+                case 0 when (todayInfo.Conditions == "Skyet"):
+                    condition = " - Neither today will you see the sun";
+                    break;
+                case 1:
+                    var yesterdayCondition = TranslateCondition(yesterdayInfo.Conditions);
+                    var todayCondition = TranslateCondition(todayInfo.Conditions);
+                    condition = $" - {yesterdayCondition} will change to {todayCondition} today";
+                    break;
+            }
+
+            return temperature + condition;
+        }
+        
+        private static string TranslateCondition(string condition)
+        {
+            var translatedCondition = "";
+            switch (condition)
+            {   
+                case "Regn":
+                    translatedCondition = "Rainy weather";
+                    break;
+                case "Sne":
+                    translatedCondition = "Snowy weather";
+                    break;
+                case "Skyet":
+                    translatedCondition = "Cloudy weather";
+                    break;
+            }
+            return translatedCondition;
         }
     }
 }
+
